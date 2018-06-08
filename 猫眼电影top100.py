479272c211cdb4064a333c62c37f8e0753b031ae
mr-tang1 <39638174+mr-tang1@users.noreply.github.com>
2018-06-08 22:40:33 +0800

    Add files via upload

---
+++ 猫眼电影top100.py
@@ -0,0 +1,53 @@
+import requests
+import re
+import json
+from requests.exceptions import RequestException
+import time
+
+#获取网页
+def get_one_page(url):
+    try:
+        headers = {
+
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
+        }
+        response = requests.get(url,headers=headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+#解析网页，正则表达式。获取需要的信息
+def parse_one_page(html):
+    patten = re.compile('<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',
+                        re.S)
+    items = re.findall(patten,html)
+    for item in items:
+        yield{
+            'index':item[0],
+            'igage':item[1],
+            'title':item[2].strip(),
+            'actor':item[3].strip()[3:] if len(item[3]) > 3 else '',
+            'time':item[4].strip()[5:] if len(item[4]) > 5 else '',
+            'score':item[5].strip()+item[6].strip()
+        }
+#将获取的信息存入text文件
+def write_to_file(content):
+    with open('result.txt','a',encoding='utf-8') as f:
+        print(type(json.dumps(content)))
+        #ensure_ascii参数为False，保证输出结果是中文形式而不是Unicode
+        f.write(json.dumps(content,ensure_ascii=False)+'\n')
+#传递url
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset='+str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+#主程序：给offset传值
+if __name__ == '__main__':
+    for i in range(10):
+        main(offset=i * 10)
+        time.sleep(1)
+
+
